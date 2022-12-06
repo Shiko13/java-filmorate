4ccbb9693d3aace13cc9a1388bb63e6e9fe9fb7a
NFilin <nickadm@yandex.ru>
2022-12-07 02:07:05 +0300

    добавлен import java.util.Collection;

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
